04c5a70a13b6fbe384a6ebe377efd3517fcc70d2
Dan Scott <Dan.Scott@sse.com>
2022-02-24 16:30:57 +0000

    global fixtures

--- cypress/integration/2-fixtures/fixtures.js
+++ cypress/integration/2-fixtures/fixtures.js
@@ -1,14 +1,15 @@
  /// <reference types="cypress" />
 
 describe("My third test suite", () => {
-  before(() => {
+  beforeEach(function () {
+    cy.fixture("example").then((bob) => {
+      this.user = bob
+    })
   })
 
-  it("my third testcase", () => {
-    cy.fixture("example").then((data) => {
-      cy.visit("https://rahulshettyacademy.com/angularpractice/")
-      cy.get("input[name='name']:nth-child(2)").type(data.name)
-      cy.get("select").select(data.gender)
-    })
+  it("my third testcase", function() {
+    cy.visit("https://rahulshettyacademy.com/angularpractice/")
+    cy.get("input[name='name']:nth-child(2)").type(this.user.name)
+    cy.get("select").select(this.user.gender)
   })
 })
